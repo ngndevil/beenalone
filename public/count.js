c49ebe55e0ee2980eba3c6837c01dcc71d2570ce
ngndevil <78074799+ngndevil@users.noreply.github.com>
2021-04-28 21:32:46 +0700

    new version

--- public/count.js
+++ public/count.js
@@ -1,8 +1,14 @@
 // Function calculate your FA time
 // And it render on the HTML DOM
+
 function myFunction() {
+    var dayinput = document.getElementById("di")
+    var value = dayinput.value
+    var year = Math.round(value%10000)
+    var month = Math.round((value/10000)%100) - 1 
+    var day = Math.round((value/1000000))
     var now = Date.now()
-    var ds = new Date(2002, 0, 1).getTime();
+    var ds = new Date(year, month, day).getTime();
     var fa = now - ds
     var d = Math.floor(fa / (1000 * 60 * 60 * 24))
     var h = Math.floor((fa % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
